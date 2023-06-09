6a69e33fa8deb05373b98aab1f52f3f99a45ad89
DakotaH96 <dakotamhensley1996@gmail.com>
2023-06-09 01:19:01 -0500

    added tailwind css and html

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const path = require('path');
-const {app, BrowserWindow } = require('electron');
+const {app, BrowserWindow, Menu } = require('electron');
 
 const isDev = process.env.NODE_ENV !== 'development';
 const isMac = process.platform === 'darwin';
 
+//Create main window
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Image Resizer',
@@ -22,8 +23,13 @@ function createMainWindow() {
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
+//App is ready
 app.whenReady().then(() => {
     createMainWindow();
+
+    //Implement menu
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
@@ -32,6 +38,12 @@ app.whenReady().then(() => {
       })
 });
 
+//Menu template
+const menu = [
+    {
+        role: 'fileMenu',
+    }
+]
 
 app.on('window-all-closed', () => {
     if (!isMac) {
